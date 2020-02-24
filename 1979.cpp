500d7cbaa7d9bc8f37ccfcf57876f805473077b8
shpark5938 <shpark5938@naver.com>
2020-02-24 21:31:40 +0900

    백준

---
+++ 1979.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int T, N, K, answer;
+	cin >> T;
+
+	for (int t_case = 1; t_case <= T; t_case++) {
+		cin >> N >> K;
+		bool puzzle[15][15];
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				cin >> puzzle[i][j];
+			}
+		}
+
+		answer = 0;
+
+		// 가로 체크
+		for (int i = 0; i < N; i++) {
+			int cnt = 0;
+			int j = 0;
+			while (true) {
+				if (!puzzle[i][j]) {
+					if (cnt == K)
+						answer++;
+					cnt = 0;
+					j++;
+				}
+				else {
+					cnt++;
+					j++;
+				}
+				if (j == N) {
+					if (cnt == K)
+						answer++;
+					break;
+				}
+			}
+		}
+
+		// 세로 체크
+		for (int i = 0; i < N; i++) {
+			int cnt = 0;
+			int j = 0;
+			while (true) {
+				if (!puzzle[j][i]) {
+					if (cnt == K)
+						answer++;
+					cnt = 0;
+					j++;
+				}
+				else {
+					cnt++;
+					j++;
+				}
+				if (j == N) {
+					if (cnt == K)
+						answer++;
+					break;
+				}
+			}
+		}
+
+		cout << "#" << t_case << " " << answer << "\n";
+	}
+
+	return 0;
+}
