55a28b72861fbb0e329d9f0e1252f573ab592362
shpark5938 <shpark5938@naver.com>
2020-02-28 17:58:56 +0900

    백준

---
+++ 1946.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int T;
+int N;
+int arr[10];
+int main()
+{
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++)
+	{
+		cin >> N;
+		int alpha, num;
+		for (int zip = 1; zip <= N; zip++)
+		{
+			cin >> alpha >> num;
+		}
+		
+
+
+	}
+	cout << '#' << tc << ' ' << endl;
+}
