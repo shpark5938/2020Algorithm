8e9053a0b27e4707d5e9be94ebba8b7ff78156d2
shpark5938 <shpark5938@naver.com>
2021-01-07 17:36:06 +0900

    programmers

---
+++ 프로그래머스/3진법뒤집기.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+	vector<int> v;
+
+	while (n)
+	{
+		v.push_back(n % 3);
+		n /= 3;
+	}
+
+	reverse(v.begin(), v.end());
+
+	for (int i = 0; i < v.size(); i++)
+		answer += pow(3, i) * v[i];
+
+	return answer;
+}
+
+int main() {
+	cout << solution(45);
+}
