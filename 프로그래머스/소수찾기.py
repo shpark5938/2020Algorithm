a58f2d885a2c4848e751904a7acec9f545094b1b
shpark5938 <shpark5938@naver.com>
2021-01-13 15:51:20 +0900

    programmers

--- 프로그래머스/소수찾기.py
+++ 프로그래머스/소수찾기.py
@@ -1,14 +1,8 @@
 def solution(n):
     answer = 0
-    s = 0
+    numset = set(range(2, n + 1))
     for i in range(2, n + 1):
-        for j in range(2, i):
-            if i % j == 0:
-                s += 1
-        if s == 0:
-            answer += 1
-        s = 0
+        if i in numset:
+            numset -= set(range(2 * i, n + 1, i))
+    answer = len(numset)
     return answer
-
-
-print(solution(10))
