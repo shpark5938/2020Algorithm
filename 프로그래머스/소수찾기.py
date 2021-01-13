abd101336535b4121969e6d1a9799ce007f730b0
shpark5938 <shpark5938@naver.com>
2021-01-13 15:31:40 +0900

    programmers

---
+++ 프로그래머스/소수찾기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+    s = 0
+    for i in range(2, n + 1):
+        for j in range(2, i):
+            if i % j == 0:
+                s += 1
+        if s == 0:
+            answer += 1
+        s = 0
+    return answer
+
+
+print(solution(10))
