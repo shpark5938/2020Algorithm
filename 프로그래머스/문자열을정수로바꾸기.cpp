51a510f66c670b951645e8594ecb83899916cc8e
shpark5938 <shpark5938@naver.com>
2021-01-14 11:26:19 +0900

    programmers

---
+++ 프로그래머스/문자열을정수로바꾸기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(string s) {
+	int answer = stoi(s);
+	return answer;
+}
+
+int main() {
+	cout << solution("-1234");
+}
