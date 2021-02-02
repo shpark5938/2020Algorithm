58b0a5b2c466514e6b39d2a7e7e8c2a6e6885738
shpark5938 <shpark5938@naver.com>
2021-02-02 17:28:24 +0900

    programmers

---
+++ test/6기_문제2.py
@@ -0,0 +1,11 @@
+a = 10
+b = (1.44,'bitamin')
+c = "비타민"
+d = [1, 2, 3, 4, 5]
+e = {'특별시':'천안','충남':'서울','인천':'광역시'}
+
+print(type(a)) # 정수형
+print(type(b)) # 튜플 형
+print(type(c)) # string 형
+print(type(d)) # list 형
+print(type(e)) # dictionary 형
