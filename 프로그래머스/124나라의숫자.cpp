68460893124bfbc83668716864a7dfb69e67f81c
shpark5938 <shpark5938@naver.com>
2021-02-05 18:22:33 +0900

    programmers

---
+++ 프로그래머스/124나라의숫자.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(int n) {
+	string answer = "";
+	string temp[3]{ "4", "1", "2" };
+	while (n) {
+		answer = temp[n % 3] + answer;
+		if (!(n % 3))
+			n = n / 3 - 1;
+		else
+			n = n / 3;
+	}
+	return answer;
+}
+
+int main() {
+	cout << solution(1);
+}
