3efb4f49d38e5f3dc60974d46bfbd8f23c91f294
박소현 <shpark5938@naver.com>
2021-05-14 02:17:03 +0900

    DELETE DNA_Algorithm

--- DNA_Algorithm/0324 배열/10818.py
+++
@@ -1,5 +0,0 @@
-num = int(input())
-arr = list(map(int, input().split()))
-arr.sort()
-
-print(arr[0], arr[num-1])
--- DNA_Algorithm/0324 배열/2577.py
+++
@@ -1,9 +0,0 @@
-a = int(input())
-b = int(input())
-c = int(input())
-
-mul = str(a * b * c)
-
-for i in range(0, 10):
-    arr = mul.count(str(i))
-    print(arr)
